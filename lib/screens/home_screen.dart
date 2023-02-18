10739ff3ca9f2c41489f63c88acfea76671564fb
hhw12409 <sksk2425@naver.com>
2023-02-18 19:21:05 +0900

    feat: video package install and setting

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,10 +5,77 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screem'),
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: getBoxDecoration(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            _Logo(),
+            SizedBox(
+              height: 30,
+            ),
+            _AppName()
+          ],
+        ),
       ),
+    );
+  }
+
+  BoxDecoration getBoxDecoration() {
+    return const BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xFF2A3A7C),
+          Color(0XFF000118),
+        ],
+      ),
+    );
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
+    );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'VIDEO',
+          style: textStyle,
+        ),
+        Text(
+          'PLAYER',
+          style: textStyle.copyWith(
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'asset/image/logo.png',
     );
   }
 }
