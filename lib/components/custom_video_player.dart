30d0aaccdf6a3c9117e21457f52b37d842702ce2
hhw12409 <sksk2425@naver.com>
2023-02-25 22:37:44 +0900

    feat: renderIconButton Widget

--- lib/components/custom_video_player.dart
+++ lib/components/custom_video_player.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
   final XFile video;
@@ -12,8 +13,75 @@ class CustomVideoPlayer extends StatefulWidget {
 }
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoController;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeController();
+  }
+
+  initializeController() async {
+    videoController = VideoPlayerController.file(
+      File(widget.video.path),
+    );
+
+    await videoController!.initialize();
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (videoController == null) {
+      return const CircularProgressIndicator();
+    }
+    return AspectRatio(
+      aspectRatio: videoController!.value.aspectRatio,
+      child: Stack(
+        children: [
+          VideoPlayer(
+            videoController!,
+          ),
+          const _Controls()
+        ],
+      ),
+    );
+  }
+}
+
+class _Controls extends StatelessWidget {
+  const _Controls({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      renderIconButton(
+        onPressed: () {},
+        iconData: Icons.rotate_left,
+      ),
+      renderIconButton(
+        onPressed: () {},
+        iconData: Icons.play_arrow,
+      ),
+      renderIconButton(
+        onPressed: () {},
+        iconData: Icons.rotate_right,
+      ),
+    ]);
+  }
+
+  Widget renderIconButton({
+    required VoidCallback onPressed,
+    required IconData iconData,
+  }) {
+    return IconButton(
+      onPressed: onPressed,
+      iconSize: 30.0,
+      color: Colors.white,
+      icon: Icon(iconData),
+    );
   }
 }
