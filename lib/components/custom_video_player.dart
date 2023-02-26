394459b44e24357398009a988f3141aa9ac636cb
hhw12409 <sksk2425@naver.com>
2023-02-26 23:42:27 +0900

    feat: play, reverse, forward function

--- lib/components/custom_video_player.dart
+++ lib/components/custom_video_player.dart
@@ -43,34 +43,103 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
           VideoPlayer(
             videoController!,
           ),
-          const _Controls()
+          _Controls(
+            onPlayPressed: onPlayPressed,
+            onReversePressed: onReversePressed,
+            onForwardPressed: onForwardPressed,
+            isPlaying: videoController!.value.isPlaying,
+          ),
+          Positioned(
+            right: 0,
+            child: IconButton(
+              onPressed: () {},
+              color: Colors.white,
+              iconSize: 30.0,
+              icon: const Icon(
+                Icons.photo_camera_back,
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
+
+  void onPlayPressed() {
+    setState(() {
+      if (videoController!.value.isPlaying) {
+        videoController!.pause();
+      } else {
+        videoController!.play();
+      }
+    });
+  }
+
+  void onReversePressed() {
+    final currentPosition = videoController!.value.position;
+
+    // default값 0초
+    Duration position = const Duration();
+    // 0초에서 3초 뒤로 가게 되는 버그 방지
+    if (currentPosition.inSeconds > 3) {
+      position = currentPosition - const Duration(seconds: 3);
+    }
+
+    videoController!.seekTo(position);
+  }
+
+  void onForwardPressed() {
+    final maxPosition = videoController!.value.duration;
+    final currentPosition = videoController!.value.position;
+
+    Duration position = maxPosition;
+    // 전체에서 3초를 뺀 길이가 현재 포지션보다 길다면 즉, 현재 포지션이 더 짧다. 이럴경우 + 3을 하여도 전체 길이를 넘지않는다.
+    if ((maxPosition - const Duration(seconds: 3)).inSeconds >
+        currentPosition.inSeconds) {
+      position = currentPosition + const Duration(seconds: 3);
+    }
+
+    videoController!.seekTo(position);
+  }
 }
 
 class _Controls extends StatelessWidget {
+  final VoidCallback onPlayPressed;
+  final VoidCallback onReversePressed;
+  final VoidCallback onForwardPressed;
+  final bool isPlaying;
+
   const _Controls({
+    required this.onPlayPressed,
+    required this.onReversePressed,
+    required this.onForwardPressed,
+    required this.isPlaying,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      renderIconButton(
-        onPressed: () {},
-        iconData: Icons.rotate_left,
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          renderIconButton(
+            onPressed: onReversePressed,
+            iconData: Icons.rotate_left,
+          ),
+          renderIconButton(
+            onPressed: onPlayPressed,
+            iconData: isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+          renderIconButton(
+            onPressed: onForwardPressed,
+            iconData: Icons.rotate_right,
+          ),
+        ],
       ),
-      renderIconButton(
-        onPressed: () {},
-        iconData: Icons.play_arrow,
-      ),
-      renderIconButton(
-        onPressed: () {},
-        iconData: Icons.rotate_right,
-      ),
-    ]);
+    );
   }
 
   Widget renderIconButton({
